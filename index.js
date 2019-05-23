01e4dd680c86e1839e679010018f536875eb316f
Pavel Kostyuk <kostyk.pavel.09@gmail.com>
2019-05-23 21:47:38 +0300

    setup dev middleware

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ require('@babel/polyfill')
 const { config: bootDotEnv } = require('dotenv')
 const App = require('./lib/App')
 const Stage = require('./lib/Stage')
-const appConfig = require('./app.config')
+const webpackConfig = require('./webpack.config')
 
 bootDotEnv()
 
-const app = new App(appConfig)
+const app = new App(webpackConfig)
 
 app.start(Stage)
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,6 @@
 const webpack = require('webpack')
 const merge = require('webpack-merge')
 const path = require('path')
-const Css = require('mini-css-extract-plugin')
-const Env = require('dotenv-webpack')
 const Stage = require('./lib/Stage')
 
 const universal = {
@@ -13,20 +11,6 @@ const universal = {
   resolve: {
     extensions: ['*', '.js', '.jsx', '.json'],
     modules: ['node_modules'],
-    alias: {
-      src: path.resolve(__dirname, './src'),
-      api: path.resolve(__dirname, './src/api'),
-      assets: path.resolve(__dirname, './src/assets'),
-      config: path.resolve(__dirname, './config'),
-      constants: path.resolve(__dirname, './src/constants'),
-      components: path.resolve(__dirname, './src/components'),
-      containers: path.resolve(__dirname, './src/containers'),
-      helpers: path.resolve(__dirname, './helpers'),
-      services: path.resolve(__dirname, './src/services'),
-      setup: path.resolve(__dirname, './setup'),
-      shapes: path.resolve(__dirname, './src/shapes'),
-      utils: path.resolve(__dirname, './src/utils')
-    }
   },
 
   context: __dirname,
@@ -38,94 +22,29 @@ const universal = {
 
   module: {
     rules: [
-      /**
-       * Resolve jsx for React components and js for all order javascript code
-       */
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
         use: ['babel-loader'],
       },
 
-      /**
-       * Looks for all css imports
-       */
-      {
-        test: /\.css$/,
-        use: [
-          Css.loader,
-          'css-loader',
-        ],
-      },
-
-      /**
-       * With this loader you can import svg icons. And this will convert svg to jsx code
-       */
-      {
-        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-        use: [
-          {
-            loader: 'react-icon-loader',
-            options: {
-              limit: 10000,
-              mimetype: 'image/svg+xml',
-            },
-          },
-        ],
-      },
-      /**
-       * With this loader you can import pictures and it will provide path to them
-       */
-      {
-        test: /\.(jpe?g|png|gif|ico)$/i,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name() {
-                return '[path][name].[ext]'
-              },
-            },
-          },
-        ],
-      },
     ],
   },
 
-  plugins: [
-
-    /**
-     * Enables support for .env files
-     */
-    new Env({ safe: true }),
-
-    /**
-     * Don't know what is's doing. Please contribute and write explanation comment :)
-     */
-    new webpack.NoEmitOnErrorsPlugin(),
-
-    /**
-     * Bundles all css to separate file
-     */
-    new Css({
-      filename: '[name].css',
-      chunkFilename: '[id].css',
-    }),
-  ]
+  plugins: []
 }
 
 const server = merge(universal, {
 
-  /**
-   * Name and mode is required for webpack to determine which mode to use
-   */
+  mode: 'development',
   name: 'server',
+
   target: 'node',
 
   entry: './src/server',
 
   output: {
-    path: path.resolve(__dirname, './build'),
+    path: path.resolve(__dirname, './dist'),
     filename: 'server.js',
     libraryTarget: 'commonjs2'
   },
@@ -144,18 +63,21 @@ const server = merge(universal, {
 
 const client = merge(universal, {
   mode: 'development',
+  name: 'client',
 
   entry: {
     client: [
-      '@babel/polyfill',
-      'webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000',
       './src/client.js',
     ],
   },
 
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-  ]
+  output: {
+    path: path.resolve(__dirname, './dist/public'),
+    publicPath: '/',
+    filename: `client.js`,
+  },
+
+  plugins: []
 })
 
 module.exports = [server, client]
